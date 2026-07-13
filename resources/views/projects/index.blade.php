463447eb8b7e9542fe3dbf529b2f13de3cf32f87
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-07-13 17:41:40 +0200

    Implement project listing functionality with index view and resource route

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,24 @@
+@extends("layouts.projects")
+
+@section("title", "I miei progetti")
+
+@section("content")
+
+<div class="row row-cols-1 row-cols-md-3">
+
+    @foreach ($projects as $project)
+
+    <div class="col">
+        <div class="card">
+            <h2 class="text-center mt-2">{{$project->nome}}</h2>
+            <div class="card-body">
+                <a href="">Visualizza</a>
+            </div>
+        </div>
+    </div>
+
+    @endforeach
+
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,6 @@ Route::middleware(["auth","verified"])
     ->name("index");
 });
 
-
+Route::resource("projects", ProjectController::class);
 
 require __DIR__.'/auth.php';
