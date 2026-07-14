1a232d8cde9816ea36e72b9c8ed11e0dd316c4b9
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-07-14 14:24:42 +0200

    Implement project deletion functionality with confirmation modal in show view

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -5,7 +5,14 @@
 @section('title', $project->nome)
 
 @section('content')
-<a class="btn btn-warning" type="submit" href="{{route("projects.edit", $project)}}">Modifica</a>
+    <a class="btn btn-warning" type="submit" href="{{ route('projects.edit', $project) }}">Modifica</a>
+
+    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Cancella
+    </button>
+
+
+
     <div class="d-flex flex-column gap-3">
         <div>
             <h3>A chi è rivolto?</h3>
@@ -23,7 +30,28 @@
         </div>
     </div>
 
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Attenzione</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Stai eliminando il progetto definitivamente. Sei sicuro?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Annulla</button>
+                    <form action="{{ route('projects.destroy', $project) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
 
+                        <input class="btn btn-danger" type="submit" value="Elimina">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
