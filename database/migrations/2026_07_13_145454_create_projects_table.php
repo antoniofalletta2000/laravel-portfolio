4f75aec00845e068736c2f124c2c2f1af038bfed
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-07-16 13:01:40 +0200

    Add type_id field to Project seeder and ensure proper assignment

--- database/migrations/2026_07_13_145454_create_projects_table.php
+++ database/migrations/2026_07_13_145454_create_projects_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string("cliente")->nullable();
             $table->date("periodo");
             $table->longText("riassunto");
+            
 
             $table->timestamps();
         });
--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -18,19 +18,22 @@ class ProjectTableSeeder extends Seeder
                 "nome" => "Booldog",
                 "cliente" => "chi cerca qualità nel prodotto per il bene del proprio animale domestico.",
                 "periodo" => "2026-06-19",
-                "riassunto" => "Piattaforma e-commerce lato cliente dedicata alla vendita di accessori di qualità per cani e gatti."
+                "riassunto" => "Piattaforma e-commerce lato cliente dedicata alla vendita di accessori di qualità per cani e gatti.",
+                "type_id" => 1
             ],
             [
                 "nome" => "Booroad",
                 "cliente" => "Agenzia di Viaggi",
                 "periodo" => "2026-05-30",
-                "riassunto" => "Applicazione pensata per gli accompagnatori che gestiscono gruppi in viaggio. L'idea è semplice: avere a portata di mano tutti i contatti dei partecipanti, organizzati per viaggio, senza dover scartabellare fogli o aprire mille file diversi."
+                "riassunto" => "Applicazione pensata per gli accompagnatori che gestiscono gruppi in viaggio. L'idea è semplice: avere a portata di mano tutti i contatti dei partecipanti, organizzati per viaggio, senza dover scartabellare fogli o aprire mille file diversi.",
+                "type_id" => 2
             ],
             [
                 "nome" => "Boolflix",
                 "cliente" => "Amanti del cinema e Serie TV",
                 "periodo" => "2026-05-27",
-                "riassunto" => "Sito pensato per coloro che ricercano intrattenimento mentre si condividono dei Pop Corn"
+                "riassunto" => "Sito pensato per coloro che ricercano intrattenimento mentre si condividono dei Pop Corn",
+                "type_id" => 3
             ]
         ];
 
@@ -41,7 +44,7 @@ class ProjectTableSeeder extends Seeder
             $newProject->cliente = $project["cliente"];
             $newProject->periodo = $project["periodo"];
             $newProject->riassunto = $project["riassunto"];
-
+            $newProject->type_id = $project["type_id"];
             $newProject->save();
         }
     }
